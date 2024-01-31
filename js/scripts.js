5505ab16ef48f8760de27881a6eefc57a428aef7
saad-ashraf-1 <158234874+sa-gwu-bootcamp@users.noreply.github.com>
2024-01-30 22:56:39 -0500

    Additional animation

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 $(document).ready(function() {
-    // Smooth scrolling for nav links
-    $('header ul li').click(function() {
-      var link = $(this).text().toLowerCase();
-      $('html, body').animate({
-        scrollTop: $('#' + link).offset().top
-      }, 1000);
-      return false;
-    });
-  
-    // Mobile menu toggle
-    $('#hamburger-menu').click(function() {
-      $('#mobile-menu-overlay').fadeToggle(500);
-    });
-  
-    // Mobile menu links
-    $('#mobile-menu-overlay ul li').click(function() {
-      var link = $(this).text().toLowerCase();
-      $('html, body').animate({
-        scrollTop: $('#' + link).offset().top
-      }, 1000);
-      $('#mobile-menu-overlay').fadeOut(500);
+
+  // Mobile menu links
+  $('#mobile-menu-overlay ul li').click(function() {
+    var target = $(this).attr('data-target');
+    var section = $('#' + target);
+  });
+
+  // Click event for 'Skills' nav item
+  $('.nav-item[data-target="skills"]').click(function(e) {
+    e.preventDefault();
+    
+    // If you want to hide all other sections
+    $('section').not('#skills').fadeOut(500);
+
+    // Now show the skills section
+    $('#skills').addClass('active-section').fadeIn(500, function() {
+      // This callback is optional and can be used to perform actions after the fade in is complete
     });
   });
+
+  // // If you have a close button in the skills section, you can use the following:
+  // $('#skills .close-button').click(function() {
+  //   $('#skills').removeClass('active-section').fadeOut(500);
+  // });
+
+  // Click event for close button
+  $('#skills .close-button').click(function() {
+    $('#skills').fadeOut(500);
+  });
+});
